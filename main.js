8067829ac3b599ab94c261d49282ff0fe025e841
John Doe <johndoe@example.com>
2023-08-21 20:15:30 +0100

    Obtained Access Token. Configuring playlist creation

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
-
+const axios = require("axios");
 const artistId = "775ad383-ac2d-479a-af9d-b561442cb749";
 
+let spotifyAccessToken;
 let artistObject = {};
 
 const fetchPlaylist = () => {
@@ -18,7 +19,7 @@ const fetchPlaylist = () => {
       let eventDate = data.setlist[0].eventDate;
       let tracklist = [];
 
-      data.setlist[0].sets.set[0].song.map((song) => tracklist.push(song.name));
+      data.setlist[1].sets.set[0].song.map((song) => tracklist.push(song.name));
       //console.log(artistName, venueName, eventDate, tracklist);
 
       artistObject = {
@@ -34,3 +35,61 @@ const fetchPlaylist = () => {
 };
 
 fetchPlaylist().then((result) => console.log(result));
+
+// Token Request
+
+const client_id = process.env.SPOTIFY_API_ID;
+const client_secret = process.env.SPOTIFY_API_SECRET;
+
+const authOptions = {
+  url: "https://accounts.spotify.com/api/token",
+  method: "post",
+  headers: {
+    Authorization: `Basic ${Buffer.from(
+      `${client_id}:${client_secret}`
+    ).toString("base64")}`,
+  },
+  params: {
+    grant_type: "client_credentials",
+  },
+};
+
+axios(authOptions)
+  .then((response) => {
+    if (response.status === 200) {
+      return (spotifyAccessToken = response.data.access_token);
+      console.log("Access_Token:", spotifyAccessToken);
+    }
+  })
+  .then((token) => {
+    return (createPlaylist = {
+      url: "https://api.spotify.com/v1/users/bill19922/playlists",
+      method: "post",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        content_type: "application/json",
+      },
+      json: {
+        name: "New playlist",
+        public: true,
+        description: "A new playlist",
+      },
+    });
+  })
+  .then((playlist) => {
+    console.log(playlist);
+    return axios(playlist);
+  })
+  .then((response) => {
+    console.log(response);
+    if (response.status === 201) {
+      let playlist = response;
+      //console.log("Playlist has been created", playlist);
+      console.log(spotifyAccessToken);
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+// Create Playlist
