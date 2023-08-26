f952489844d0d0897b640210b341ed0b23f4398c
AHardiment <99344068+AHardiment@users.noreply.github.com>
2023-08-26 13:30:18 +0100

    successfully implemented playlist creation

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
 const axios = require("axios");
-const artistId = "775ad383-ac2d-479a-af9d-b561442cb749";
+const qs = require("querystring");
+const base64 = require("base64-url");
 
-let spotifyAccessToken;
+const artistId = "775ad383-ac2d-479a-af9d-b561442cb749";
+const client_id = process.env.SPOTIFY_API_ID;
+const client_secret = process.env.SPOTIFY_API_SECRET;
+const auth_code = process.env.SPOTIFY_AUTH_CODE;
+const authorize = "https://accounts.spotify.com/authorize?";
+const scopes = "playlist-modify-public playlist-modify-private";
+const redirect_uri = "https://www.google.co.uk";
+const user_id = "hardimehnt";
+const authURL =
+  "https://accounts.spotify.com/authorize?client_id=83b22c59449c47c0a7755d3427a6de25&response_type=code&redirect_uri=https://www.google.co.uk&show_dialog=true&scope=playlist-modify-public playlist-modify-private";
+
 let artistObject = {};
+const newPlaylist = {
+  name: "Test playlist",
+  description: "A test playlist 1",
+  public: true,
+};
 
 const fetchPlaylist = () => {
   return fetch(`https://api.setlist.fm/rest/1.0/artist/${artistId}/setlists`, {
@@ -14,12 +30,12 @@ const fetchPlaylist = () => {
   })
     .then((response) => response.json())
     .then((data) => {
-      let artistName = data.setlist[0].artist.name;
-      let venueName = data.setlist[0].venue.name;
-      let eventDate = data.setlist[0].eventDate;
+      let artistName = data.setlist[2].artist.name;
+      let venueName = data.setlist[2].venue.name;
+      let eventDate = data.setlist[2].eventDate;
       let tracklist = [];
 
-      data.setlist[1].sets.set[0].song.map((song) => tracklist.push(song.name));
+      data.setlist[2].sets.set[0].song.map((song) => tracklist.push(song.name));
       //console.log(artistName, venueName, eventDate, tracklist);
 
       artistObject = {
@@ -38,58 +54,84 @@ fetchPlaylist().then((result) => console.log(result));
 
 // Token Request
 
-const client_id = process.env.SPOTIFY_API_ID;
-const client_secret = process.env.SPOTIFY_API_SECRET;
+const authUser = () => {
+  let url = authorize;
+  url += `client_id=${client_id}`;
+  url += "&response_type=code";
+  url += "&redirect_uri=https://www.google.co.uk";
+  url += "&show_dialog=true";
+  url += `&scope=${scopes}`;
 
-const authOptions = {
-  url: "https://accounts.spotify.com/api/token",
-  method: "post",
-  headers: {
-    Authorization: `Basic ${Buffer.from(
-      `${client_id}:${client_secret}`
-    ).toString("base64")}`,
-  },
-  params: {
-    grant_type: "client_credentials",
-  },
+  console.log(url);
 };
 
-axios(authOptions)
-  .then((response) => {
-    if (response.status === 200) {
-      return (spotifyAccessToken = response.data.access_token);
-      console.log("Access_Token:", spotifyAccessToken);
-    }
-  })
-  .then((token) => {
-    return (createPlaylist = {
-      url: "https://api.spotify.com/v1/users/bill19922/playlists",
-      method: "post",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        content_type: "application/json",
-      },
-      json: {
-        name: "New playlist",
-        public: true,
-        description: "A new playlist",
-      },
+// const getAuthCode = async () => {
+//   try {
+//     const response = await axios.get(`${authURL}`);
+
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// getAuthCode();
+
+const getAccessToken = async (authorizationCode) => {
+  try {
+    const tokenData = qs.stringify({
+      grant_type: "authorization_code",
+      code: encodeURIComponent(authorizationCode),
+      redirect_uri,
     });
-  })
-  .then((playlist) => {
-    console.log(playlist);
-    return axios(playlist);
-  })
-  .then((response) => {
-    console.log(response);
-    if (response.status === 201) {
-      let playlist = response;
-      //console.log("Playlist has been created", playlist);
-      console.log(spotifyAccessToken);
-    }
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+
+    const authHeader = Buffer.from(`${client_id}:${client_secret}`).toString(
+      "base64"
+    );
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      tokenData,
+      {
+        headers: {
+          Authorization: `Basic ${authHeader}`,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    return response.data.access_token;
+  } catch (error) {
+    console.log("Error getting access token:", error.message);
+    throw error;
+  }
+};
+
+const createPlaylist = async (playlist) => {
+  const access_token = await getAccessToken(auth_code);
+  console.log(access_token);
+  const api_url = `https://api.spotify.com/v1/users/${user_id}/playlists`;
+
+  try {
+    const response = await axios.post(api_url, JSON.stringify(playlist), {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+      data: newPlaylist,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // Create Playlist
+
+createPlaylist(newPlaylist)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error.response.data.error_description);
+  });
